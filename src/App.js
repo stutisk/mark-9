1c0ac61a27c6af7ed579ebce82d36a00fdb1e95a
stutisk <48529714+stutisk@users.noreply.github.com>
2021-09-14 23:49:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,81 +6,89 @@ const hpDb = {
     {
       name: "Tara Devi Temple",
       description:
-        "Tara Devi is one of the oldest temples in Shimla and one of the important religious sites in Himachal Pradesh. This temple is 250 years old."
-    },
+        "Tara Devi is one of the oldest temples in Shimla and one of the important religious sites in Himachal Pradesh. This temple is 250 years old.",
+        rating: "4.9/5 "},
     {
       name: "Jakhoo Templee",
       description:
-        "It is at a height of 8000-feet from the sea level in Shimla and has a huge Hanuman Temple. The statue of Lord Hanuman is an important landmark here."
-    },
+        "It is at a height of 8000-feet from the sea level in Shimla and has a huge Hanuman Temple. The statue of Lord Hanuman is an important landmark here.",
+        rating: "5/5 "},
     {
       name: "Hidimba Temple",
       description:
-        "One of the most famous temples to visit in Manali is the Hidimba Temple. In fact, this temple is also a popular tourist attraction for international and national tourists. It is famous for photography, pristine natural beauty, and religious institutions."
-    },
+        "One of the most famous temples to visit in Manali is the Hidimba Temple. In fact, this temple is also a popular tourist attraction for international and national tourists. It is famous for photography, pristine natural beauty, and religious institutions.",
+        rating: "4.9/5 "},
     {
       name: "Vashisht Temple",
       description:
-        "Vashisht Temple in Himachal Pradesh is famous for its natural hot springs or sulphur springs. This temple is said to be dedicated to sage Vashisht who came here to meditate. This temple provides separate bathing zones both for men and women."
-    },
+        "Vashisht Temple in Himachal Pradesh is famous for its natural hot springs or sulphur springs. This temple is said to be dedicated to sage Vashisht who came here to meditate. This temple provides separate bathing zones both for men and women.",
+        rating: "4.9/5 " },
     {
       name: "Naina Devi Ji",
       description:
-        "It is said that in order to pacify Lord Shiva, Lord Vishnu cut Maa Durga (Sati) into 50 pieces and one of her eyes fell here and this is the exact place where the shrine of Naina Devi Ji was built."
-    }
+        "It is said that in order to pacify Lord Shiva, Lord Vishnu cut Maa Durga (Sati) into 50 pieces and one of her eyes fell here and this is the exact place where the shrine of Naina Devi Ji was built.",
+        rating: "4.5/5 " }
   ],
   Cafes: [
     {
       name: "Café 1947- Manali",
       description:
-        "Located in the quite lanes of Old Manali, decorated with dimly-lit lighting the Café 1947 is famous for serving the delectable continental, Indian, and Israeli cuisines. It is one of the oldest music cafés of Manali which hosts live performances quite frequently.."
-    },
+        "Located in the quite lanes of Old Manali, decorated with dimly-lit lighting the Café 1947 is famous for serving the delectable continental, Indian, and Israeli cuisines. It is one of the oldest music cafés of Manali which hosts live performances quite frequently..",
+       rating: "4.9/5 "
+      },
     {
       name: "Hangout-Rooftop Bar & Restaurant- Kasauli",
       description:
-        "Located on the roof top of the Hotel Kasauli Regency, with a serene seating. The ambiance and the décor of the café are amazing lit-up with sparkling lighting arrangements. The place is best for its different types of domestic, imported, mixed, brewed, red and white drinks"
-    },
+        "Located on the roof top of the Hotel Kasauli Regency, with a serene seating. The ambiance and the décor of the café are amazing lit-up with sparkling lighting arrangements. The place is best for its different types of domestic, imported, mixed, brewed, red and white drinks",
+        rating: "4.5/5 "
+      },
     {
       name: "Jim Morrison Cafe - Kasol",
       description:
-        "If you like artwork on walls, cheap food, chilling, lying down in one place for a long time, ginger black tea, a decent WiFi connection right in the middle of the picturesque Kasol, then this is your hide"
-    },
+        "If you like artwork on walls, cheap food, chilling, lying down in one place for a long time, ginger black tea, a decent WiFi connection right in the middle of the picturesque Kasol, then this is your hide",
+        rating: "4.2/5 "},
     {
       name: "Cafe Simla Times - Shimla",
       description:
-        "The ambience is artsy and will surely transport you to a different world all together."
-    },
+        "The ambience is artsy and will surely transport you to a different world all together.",
+        rating: "4.0/5 " },
     {
       name: " Shiva Cafe - McLeod Ganj",
       description:
-        "I haven't known a single person who has been to McLeod Ganj and hasn't been to Shiva Cafe."
+        "I haven't known a single person who has been to McLeod Ganj and hasn't been to Shiva Cafe.",
+        rating: "5/5 "
     }
   ],
   Treks: [
     {
       name: "Kheerganga Trek",
       description:
-        "Trekking Days: 1-2 days,  Maximum-Altitude 9,711ft ,  Difficulty Level: Easy"
+        "Trekking Days: 1-2 days,  Maximum-Altitude 9,711ft ,  Difficulty Level: Easy",
+        rating: "4/5"
     },
     {
       name: "Malana Village Trek",
       description:
-        "Trekking Days: 6 days , Maximum Altitude: 10,000 ft , Difficulty Level: Moderate"
+        "Trekking Days: 6 days , Maximum Altitude: 10,000 ft , Difficulty Level: Moderate",
+        rating: "5/5 "
     },
     {
       name: "Prashar Lake Trek",
       description:
-        "Trekking Days: 1-2 days , Maximum Altitude: 8,960 ft, Difficulty Level: Easy"
+        "Trekking Days: 1-2 days , Maximum Altitude: 8,960 ft, Difficulty Level: Easy",
+        rating: "3.9/5 "
     },
     {
       name: "Chandrakhani Pass Trek",
       description:
-        "Trekking Days: 3 days , Maximum Altitude: 12,000 ft , Difficulty Level: Easy."
+        "Trekking Days: 3 days , Maximum Altitude: 12,000 ft , Difficulty Level: Easy.",
+        rating: "3/5 "
     },
     {
       name: " Hampta Pass Trek",
       description:
-        "Trekking Days: 5 days ,Maximum Altitude: 13,500 ft ,Difficulty Level: Easy to Moderate."
+        "Trekking Days: 5 days ,Maximum Altitude: 13,500 ft ,Difficulty Level: Easy to Moderate.",
+        rating: "4/5 "
     }
   ]
 };
@@ -94,6 +102,7 @@ export default function App() {
         <div key={Activities.name} className="block">
           <h2>{Activities.name}</h2>
           <p>{Activities.description}</p>
+          <h4>{Activities.rating}</h4>
         </div>
       );
     })
@@ -105,6 +114,7 @@ export default function App() {
         <div key={selectedActivity.name} className="block">
           <h2>{selectedActivity.name}</h2>
           <p>{selectedActivity.description}</p>
+          <h4>{selectedActivity.rating }</h4>
         </div>
       );
     });
